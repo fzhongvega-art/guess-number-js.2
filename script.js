dc4c5c143727bf55a173b40bec7708c348193917
fuenojon <fzhongvega@gmail.com>
2026-05-09 15:42:14 -0500

    features: sistema de nombre edad y clase implementado con js

---
+++ script.js
@@ -0,0 +1,18 @@
+let nombrePropio = "Fu Hong"; 
+
+let apellidoPaterno = "Zhong";
+
+const CURSO= "Code 101";
+
+
+
+let saludo1 = "Hola me llamo "+ " " +  nombrePropio + " " + apellidoPaterno + " " + "y estoy en el curso "+ CURSO;
+
+let saludoModerno = 'Hola me llamo ${nombrePropio} ${apellidoPaterno} y estoy en el curso ${CURSO}'
+
+console.log(saludo1);
+
+let nombrePropio2 = "Fu Hong"; 
+let edad = "17"
+let saludo3 = "hola " + nombrePropio2 + " tienes " + edad + " años"
+console.log(saludo3)
